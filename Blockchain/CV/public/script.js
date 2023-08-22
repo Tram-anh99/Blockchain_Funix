a4c3a116955ca23a8139d0b069330e0019d3e7a9
Tram-anh99 <tramanh84265@gmail.com>
2023-08-22 16:25:47 +0700

    .

---
+++ Blockchain/CV/public/script.js
@@ -0,0 +1,4 @@
+// JavaScript is not necessarily required for basic sticky headers,
+// but you can use it to add additional functionality if needed.
+// For example, changing the header style on scroll or adding
+// smooth scrolling to navigation links.
